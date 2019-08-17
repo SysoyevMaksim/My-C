fdfede2cc4677d6b5c3810ac011f58bfff1ee3c0
SysoyevMaksim <54216957+SysoyevMaksim@users.noreply.github.com>
2019-08-17 22:07:23 +0300

    Add files via upload

---
+++ znak/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double a;
+    cin>> a;
+    if (a > 0)
+    {
+     cout<< 1;
+    }
+    else
+    {
+     if (a = 0)
+     {
+      cout<< 0;
+     }
+     else
+     {
+      if( a < 0)
+      {
+       cout<< -1;
+      }
+     }
+    }
+    return 0;
+}
