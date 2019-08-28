dd4a12e7a6f600676dfaf1f01cbaeaed10cbf7a3
maksim <sysoyev.maxim@gmail.com>
2019-08-28 12:56:21 +0300

    Первая поправка шариков

--- shariki 2/main.cpp
+++ shariki 2/main.cpp
@@ -14,16 +14,22 @@ int main()
     {
         if(array[x] < 0)
         {
-            continue;
+            break;
         }
         if(array[x] == array[x + 1] && array[x] == array[x + 2])
         {
            c = x;
-           for(int y = x + 2; array[y] == array [x]; y++)
+           for(int y = x + 2; array[y] == array [x] && y < n; y++)
            {
               d = y;
            }
            e = d - c + 1;
+           if(d == n - 1)
+           {
+               kol+= e;
+               cout<< e;
+               return 0;
+           }
            for(int z = d + 1; z < n; z++)
            {
                array[z - e] = array[z];
